f6ebb9fa9b3f9a253a7cf3d4bd3d1f0e17bc05a9
HlufD <hlufabebe2015@gmail.com>
2025-05-21 14:34:08 +0300

    add basic heath check

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
 // this is a version
 const version = "1.0.0"
 
-// this is config for the app ,like port and eviromet
-type Config struct {
+// this is config for the app ,like port and environment
+type config struct {
 	port int
 	env  string
 }
 
 // This is a struct that holds dependencies for the application like  database, for now only config and logger
-type Appilcation struct {
-	config Config
+type Application struct {
+	config config
 	logger *log.Logger
 }
 
 func main() {
 
-	fmt.Println("Hello world")
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "This the port on which the app runs")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &Application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", cfg.port),
+		Handler:           mux,
+		ReadTimeout:       10 * time.Second,
+		IdleTimeout:       time.Minute,
+		ReadHeaderTimeout: 30 * time.Second,
+	}
+
+	logger.Printf("The server is running on port: %d", cfg.port)
+
+	err := server.ListenAndServe()
+
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
